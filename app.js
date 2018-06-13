04a4c8b38156d28a86fbf868d481be46f089ee28
Seyedpouya <pjavadi84@gmail.com>
2018-06-13 02:37:26 +0000

    updated app.js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes =    require("./routes/comments"),
     indexRoutes =       require("./routes/index");
     
 // Executing variables
-mongoose.connect("mongodb://localhost/yelp_camp_v13deployed");// 
-// mongoose.connect("mongodb://yelpcamp:yelpcamp123@ds257640.mlab.com:57640/yelpcamp");
+// mongoose.connect("mongodb://localhost/yelp_camp_v13deployed");// 
+mongoose.connect("mongodb://yelpcamp:yelpcamp123@ds257640.mlab.com:57640/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
