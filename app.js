4d1542238c4e506741fa897f91b7a6f0d8eded8c
Seyedpouya <pjavadi84@gmail.com>
2018-06-13 05:55:48 +0000

    update app.js

--- app.js
+++ app.js
@@ -58,4 +58,4 @@ app.use("/campgrounds",campgroundRoutes),
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP, function(){});
