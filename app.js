dc54c898bfb28d6f2b3cf3fcbb0ffb9759fa85d3
Seyedpouya <pjavadi84@gmail.com>
2018-06-13 05:57:34 +0000

    update app.js

--- app.js
+++ app.js
@@ -58,4 +58,6 @@ app.use("/campgrounds",campgroundRoutes),
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){});
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("yelpcamp server has started!")
+});
